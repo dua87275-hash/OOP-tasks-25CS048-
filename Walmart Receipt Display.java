9c89fc94b11ab48147185fcb1f8787dae6c2bb2d
dua87275-hash <dua87275@gmail.com>
2026-03-13 22:29:47 -0700

    Add files via upload

---
+++ Walmart Receipt Display.java
@@ -0,0 +1,16 @@
+class prc{
+    public static void main(String[] args){
+                String storeName = "walmart";
+        String productName = "skinny jeans";
+        int price = 103;
+        int quantity = 50;
+        int totalCost= price * quantity;
+
+        System.out.println("The name of store is : " + storeName);
+        System.out.println("The product bought is : " + productName);
+        System.out.println("The price of a product is : " + price);
+        System.out.println("The Quantity of that product is : " + quantity);
+        System.out.println("The total bill is : " + totalCost);
+
+    }
+}
