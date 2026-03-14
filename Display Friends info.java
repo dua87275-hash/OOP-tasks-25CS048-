c2ddf11ffa6f752eba784effbc61f043d2b8288a
dua87275-hash <dua87275@gmail.com>
2026-03-13 22:28:48 -0700

    Add files via upload

---
+++ Display Friends info.java
@@ -0,0 +1,30 @@
+
+public class prc2{
+    public static void main(String[] args){
+        String friendName1 = "sadia" ;
+        String emoji1 = "\uD83D\uDE01";
+        String tagline1 = "sahi baat hai"; 
+        
+        System.out.println(friendName1);
+        System.out.println(emoji1);
+        System.out.println(tagline1);
+
+         String friendName2 = "Amara" ;
+        String emoji2 = "\uD83D\uDE01";
+        String tagline2 = "Neend arahi hai"; 
+        
+        System.out.println(friendName2);
+        System.out.println(emoji2);
+        System.out.println(tagline2);
+
+         String friendName3 = "Hurriya" ;
+        String emoji3 = "\uD83D\uDE01";
+        String tagline3 = "Mne kuch nahi parha yaar"; 
+        
+        System.out.println(friendName3);
+        System.out.println(emoji3);
+        System.out.println(tagline3);
+
+  }
+
+}
